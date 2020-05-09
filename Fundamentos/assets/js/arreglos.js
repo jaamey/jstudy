3a66a24dbbf2c01413858c5c2f20a44996dde0e7
Masco Ticas <jaamey@gmail.com>
2020-05-09 17:29:40 -0600

    xxx

---
+++ Fundamentos/assets/js/arreglos.js
@@ -0,0 +1,29 @@
+// const arr = new Array(10);
+// console.log(arr);
+
+// const arr2 = [];
+// console.log(arr2);
+
+
+let videoJuegos = ['Mario 3','Megaman','Chrono Trigger'];
+console.log({videoJuegos});
+
+console.log(videoJuegos[0]);
+
+let arregloCosas = [
+    true,
+    123,
+    'Fernando',
+    1+2,
+    function(){},
+    ()=>{},
+    { a:1 },
+    ['X','Megaman','Zero','DR. Light',[
+       'Dr. Willy',
+       'woodman' 
+    ]]
+];
+
+console.log(arregloCosas[2]);
+console.log( arregloCosas[7][4][1] );
+
---
+++ Fundamentos/assets/js/primitivos.js
@@ -0,0 +1,30 @@
+let nombre = 'Peter Parker';
+console.log(nombre);
+nombre = 'Ben Parker';
+console.log(nombre);
+
+nombre = "Tia May";
+
+console.log (typeof nombre);
+
+let esMarvel = true;
+console.log (typeof esMarvel);
+
+let edad = 33;
+console.log(edad);
+
+edad = 33;
+console.log( typeof edad);
+
+let superPoder;
+console.log( typeof superPoder);
+
+let soyNull = null;
+console.log( typeof null);
+
+let symbol1 = Symbol('a');
+let symbol2 = Symbol('a');
+
+console.log( typeof symbol1);
+console.log( symbol1 === symbol2);
+
