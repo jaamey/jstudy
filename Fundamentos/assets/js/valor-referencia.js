025ee2bb3028bfb8747c9b7b560817dca3413bc8
Masco Ticas <jaamey@gmail.com>
2020-05-23 15:09:19 -0600

    starting seccion4

---
+++ Fundamentos/assets/js/valor-referencia.js
@@ -0,0 +1,40 @@
+
+let a = 10;
+let b = a ;
+a = 30;
+
+console.log ({a,b});
+
+let juan = { nombre : 'Juan'};
+let ana = { ...juan };
+ana.nombre='ana';
+
+console.log ({juan, ana});
+
+const cambiaNombre = ( {...persona} ) => {
+    persona.nombre = 'Tony';
+    return persona;
+}
+
+let peter = { nombre: 'Peter'};
+let tony = cambiaNombre( peter);
+
+console.log ({ peter, tony });
+
+//arreglo
+const frutas =['Manzana','Pera','Piña'];
+//const otrasFrutas = [...frutas];
+console.time('slice');
+const otrasFrutas = frutas.slice();
+console.timeEnd('slice');
+
+console.time('spread');
+const otrasFrutas2 = [...frutas];
+console.timeEnd('spread');
+
+otrasFrutas.push('Mango')
+console.table({ frutas, otrasFrutas });
+
+
+
+
